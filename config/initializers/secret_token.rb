cd5f536c415fe4d0d19f08a694b2ec31f97f51c6
Jerry Lee <jcrt@pixnet.tw>
2011-07-22 17:50:00 +0800

    fix Rails::Application

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Book::Application.config.secret_token = '50bb5145b873780226e6dd24ecf71915166f7caabf1eae108508b0bf96e8beb8b62f7ca2a7861cefe93df6d113c9afd8d256298d1a6578502b6c96a48775b202'
+Rails.application.config.secret_token = '50bb5145b873780226e6dd24ecf71915166f7caabf1eae108508b0bf96e8beb8b62f7ca2a7861cefe93df6d113c9afd8d256298d1a6578502b6c96a48775b202'
